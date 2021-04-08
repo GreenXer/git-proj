0b965962d5c30ac8b46e86b3f70f87399cb9423d
Costi Cocolea <costipro9@gmail.com>
2021-04-08 17:13:57 +0300

    move main

--- Terminatorul/main.cpp
+++ Terminatorul/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
 
 using namespace std;
 
+int tinta = 30;
+
+void setup()
+{
+    cout << "D>>";
+    for(int i = 1; i < tinta; i++)
+        cout << " ";
+    cout << "O A" << endl;
+}
+
+void core()
+{
+    int x;
+    setup();
+    cin >> x;
+    cout << endl;
+    system("CLS");
+    setup();
+    cout << "D>>";
+    for(int i = 1; i < x; i++)
+    {
+        if(i == tinta || i == tinta + 2)
+            cout << "&";
+        else
+            cout << "-";
+        Sleep(40);
+    }
+    cout << "X" << endl;
+
+    if(x == tinta || x == tinta + 1)
+        cout << "Ai castigat" << endl;
+    else
+        cout << "Ai pierdut" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    core();
+    getch();
+    system("CLS");
+    return main();
 }
